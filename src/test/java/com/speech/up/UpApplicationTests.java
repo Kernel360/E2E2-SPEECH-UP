2b9004b59a878ca5f99e0aa38782c974907c9902
Lee kangmin <88952924+km2535@users.noreply.github.com>
2024-07-29 17:42:11 +0900

    초기 세팅 파일
    초기 세팅 파일입니다. 패치 요망

---
+++ src/test/java/com/speech/up/UpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speech.up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
