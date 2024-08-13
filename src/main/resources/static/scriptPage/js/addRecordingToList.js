06b441d2ed33394f7e19dbfd8f804931f63ab380
이재윤 <kjd99002@naver.com>
2024-08-13 12:34:11 +0900

    [#82] 녹음 추가 (#83)
    * [#82] 녹음 추가

--- src/main/resources/static/scriptPage/js/addRecordingToList.js
+++ src/main/resources/static/scriptPage/js/addRecordingToList.js
@@ -1,20 +1,27 @@
+let i = 1;
 
 function addRecordingToList(url, file) {
     const list = document.getElementById('recordings-list');
     const li = document.createElement('li');
 
-    // Create a new button and set up the event listener
-    const button = document.createElement('button');
-    button.textContent = '저장하기';
-    button.classList.add("saving-button");
-    button.addEventListener('click', () => saveRecord(file));
 
+    // Number(i)
+    li.id = 'record-' + i;
+    // Create a new button and set up the event listener
     li.innerHTML = `
                 <div class="recording-item">
                     <audio controls src="${url}"></audio>
+                   
                 </div>
             `;
+    const button = document.createElement('button');
+
+    button.textContent = '분석하기';
+    button.classList.add("saving-button");
+    button.addEventListener('click', () => saveRecord(file, li.id))
     li.appendChild(button);
     list.appendChild(li);
+
+    i++;
 
 }
--- src/main/resources/static/scriptPage/js/saveRecord.js
+++ src/main/resources/static/scriptPage/js/saveRecord.js
@@ -1,19 +1,24 @@
-
 // 오디오 분석
-function saveRecord(blob) {
+function saveRecord(blob, elementId) {
     // blob 을 FormData 에 직접 추가
     const urlParams = new URLSearchParams(window.location.search);
-    const content =  document.getElementById('script-content').innerHTML;
-    const directory = "/Users/leekangmin/Documents/E2E/E2E2-SPEECH-UP"; // 개인 아이디로 변경
+    const content = document.getElementById(elementId).innerHTML;
+    const directory = "/Users/jaeyunlee/Documents/E2E/E2E2-SPEECH-UP"; // 개인 아이디로 변경
 
     const formData = new FormData();
-    const body = {script_id : urlParams.get('scriptId') };
+    //유저네임 없으면 1보냄 나중에 삭제필요
+    const body = {
+        script_id: 1,
+        user_id: 1
+    };
     const jsonBody = JSON.stringify(body);
     formData.append('script', jsonBody);
     formData.append('audioPath', directory);
     formData.append('languageCode', document.getElementById('language-select').value);
     formData.append('file', blob, blob.name);
     const button = document.getElementsByClassName("saving-button");
+
+
     fetch('/speech-record', {
         method: 'POST',
         body: formData
