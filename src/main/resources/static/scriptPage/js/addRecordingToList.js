3bab14364a6b3301f25e994522e22ca611b2ab2f
gooot <kjd99002@naver.com>
2024-08-13 12:31:15 +0900

    [#82] 녹음 추가

--- src/main/resources/static/scriptPage/js/addRecordingToList.js
+++ src/main/resources/static/scriptPage/js/addRecordingToList.js
@@ -1,17 +1,13 @@
-let i=1;
+let i = 1;
+
 function addRecordingToList(url, file) {
     const list = document.getElementById('recordings-list');
     const li = document.createElement('li');
 
 
-
     // Number(i)
-    li.id='record-'+i;
-
-    console.log(("li.id : "+li.id))
-
+    li.id = 'record-' + i;
     // Create a new button and set up the event listener
-    console.log(file);
     li.innerHTML = `
                 <div class="recording-item">
                     <audio controls src="${url}"></audio>
@@ -22,7 +18,7 @@ function addRecordingToList(url, file) {
 
     button.textContent = '분석하기';
     button.classList.add("saving-button");
-    button.addEventListener('click', () => saveRecord(file,li.id))
+    button.addEventListener('click', () => saveRecord(file, li.id))
     li.appendChild(button);
     list.appendChild(li);
 
--- src/main/resources/static/scriptPage/js/saveRecord.js
+++ src/main/resources/static/scriptPage/js/saveRecord.js
@@ -1,25 +1,15 @@
-
 // 오디오 분석
-function saveRecord(blob,elementId) {
+function saveRecord(blob, elementId) {
     // blob 을 FormData 에 직접 추가
     const urlParams = new URLSearchParams(window.location.search);
-    const content =  document.getElementById(elementId).innerHTML;
+    const content = document.getElementById(elementId).innerHTML;
     const directory = "/Users/jaeyunlee/Documents/E2E/E2E2-SPEECH-UP"; // 개인 아이디로 변경
 
-    console.log("blob : "+blob.name);
-    console.log("blob : "+blob.toString());
-
-    const element = document.getElementById(elementId); // 확인할 요소의 ID
-    console.log(element); // 요소가 null인지 확인
-    if (element) {
-        element.innerHTML = '파일이 저장되었습니다.'; // 예시로 innerHTML 설정
-    } else {
-        console.error('요소를 찾을 수 없습니다.');
-    }
     const formData = new FormData();
     //유저네임 없으면 1보냄 나중에 삭제필요
-    const body = {script_id : 1,
-    user_id : 1
+    const body = {
+        script_id: 1,
+        user_id: 1
     };
     const jsonBody = JSON.stringify(body);
     formData.append('script', jsonBody);
@@ -27,13 +17,6 @@ function saveRecord(blob,elementId) {
     formData.append('languageCode', document.getElementById('language-select').value);
     formData.append('file', blob, blob.name);
     const button = document.getElementsByClassName("saving-button");
-
-    console.log("formData : "+formData.get('script').toString());
-    console.log("formData : "+formData.get('audioPath').toString());
-    console.log("formData : "+formData.get('languageCode').toString());
-    console.log("formData : "+formData.get('file').toString());
-
-
 
 
     fetch('/speech-record', {
