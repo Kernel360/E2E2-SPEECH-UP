3c57fa905bdda97d7204ff401070a413def2a52f
Lee kangmin <km2535@naver.com>
2024-08-29 11:39:02 +0900

    [#54] env 관련 테스트 수정

--- src/test/java/com/speech/up/demo/HomePageControllerTest.java
+++ src/test/java/com/speech/up/demo/HomePageControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
